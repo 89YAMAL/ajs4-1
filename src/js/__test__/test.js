cf641863a9c9e769a11c54a6b53c63fc166eb68a
89YAMAL <alex-popov1987@yandex.ru>
2022-09-16 18:19:18 +0500

    commit3

--- src/js/__test__/test.js
+++ src/js/__test__/test.js
@@ -2,7 +2,7 @@ import getHealth from '../game';
 
 test.each([
   { name: 'Маг', health: 100, expected: 'healthy' },
-  { name: 'Маг', health: 70, expected: 'healthy' },
+  { name: 'Маг', health: 75, expected: 'healthy' },
   { name: 'Маг', health: 50, expected: 'wounded' },
   { name: 'Маг', health: 30, expected: 'wounded' },
   { name: 'Маг', health: 15, expected: 'wounded' },
